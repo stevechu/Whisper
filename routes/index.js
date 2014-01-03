ebc5661738937e07e21a542b5f384e6745824fbd
stevechu <chujinsun@gmail.com>
2014-01-03 09:54:37 +0800

    add blog remove

--- routes/index.js
+++ routes/index.js
@@ -205,6 +205,20 @@ module.exports = function(app) {
       res.redirect(url);
     });
   });
+
+  app.get('/remove/:name/:day/:title', checkLogin);
+  app.get('/remove/:name/:day/:title',checkUser);
+  app.get('/remove/:name/:day/:title', function (req, res) {
+    var currentUser = req.session.user;
+    Post.remove(currentUser.name, req.params.day, req.params.title, function (err) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect(url);
+      }
+      req.flash('success', '删除成功!');
+      res.redirect('/');
+    });
+  });
   
   function checkLogin(req,res,next){
     if(!req.session.user){
